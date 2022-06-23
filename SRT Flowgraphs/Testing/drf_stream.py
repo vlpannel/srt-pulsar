0865cf450b304af14dba35cfeaf075b749eeec88
vlpannel <103673975+vlpannel@users.noreply.github.com>
2022-06-23 11:30:51 -0400

    Attempted to debug DRF.
    Seems that DRF only outputs empty things--not sure if I'm using it right.

---
+++ SRT Flowgraphs/Testing/drf_stream.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Testing/Debugging DRF Streams
+# Author: vivelpanel
+# GNU Radio version: 3.9.5.0
+
+from distutils.version import StrictVersion
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print("Warning: failed to XInitThreads()")
+
+from gnuradio import blocks
+from gnuradio import gr
+from gnuradio.filter import firdes
+from gnuradio.fft import window
+import sys
+import signal
+from PyQt5 import Qt
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+import gr_digital_rf
+import numpy as np; import gr_digital_rf
+
+
+
+from gnuradio import qtgui
+
+class drf_stream(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Testing/Debugging DRF Streams", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Testing/Debugging DRF Streams")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "drf_stream")
+
+        try:
+            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+                self.restoreGeometry(self.settings.value("geometry").toByteArray())
+            else:
+                self.restoreGeometry(self.settings.value("geometry"))
+        except:
+            pass
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 1e6
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.gr_digital_rf_digital_rf_source_0 = gr_digital_rf.digital_rf_source(
+            "/Volumes/NO Name/pulsar/2022-05-26/rf_data",
+            channels=[
+                'misa-l2',
+            ],
+            start=[
+                165359520000,
+            ],
+            end=[
+                165360239999,
+            ],
+            repeat=True,
+            throttle=True,
+            gapless=False,
+            min_chunksize=None,
+        )
+        self.gr_digital_rf_digital_rf_sink_0 = gr_digital_rf.digital_rf_sink(
+            '/Users/vivelpanel/Desktop/SRT UROP 2022/SRT Flowgraphs/Testing Output Files/drf_sink_out',
+            channels=[
+                'ch0',
+            ],
+            dtype=np.dtype([("r", "i2"), ("i", "i2")]),
+            subdir_cadence_secs=3600,
+            file_cadence_millisecs=1000,
+            sample_rate_numerator=int(samp_rate),
+            sample_rate_denominator=1,
+            start=None,
+            ignore_tags=False,
+            is_complex=True,
+            num_subchannels=1,
+            uuid_str=None,
+            center_frequencies=(
+                None
+            ),
+            metadata={},
+            is_continuous=True,
+            compression_level=0,
+            checksum=False,
+            marching_periods=True,
+            stop_on_skipped=False,
+            stop_on_time_tag=False,
+            debug=False,
+            min_chunksize=None,
+        )
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
+        self.blocks_null_source_0 = blocks.null_source(gr.sizeof_gr_complex*1)
+        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_gr_complex*1)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_null_source_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_null_sink_0, 0))
+        self.connect((self.gr_digital_rf_digital_rf_source_0, 0), (self.gr_digital_rf_digital_rf_sink_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "drf_stream")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+
+
+
+
+def main(top_block_cls=drf_stream, options=None):
+
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
