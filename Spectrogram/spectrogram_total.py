8cc01c7e4e762885ea8e554b32424976206a4e6d
vlpannel <103673975+vlpannel@users.noreply.github.com>
2022-06-23 13:33:10 -0400

    Started python spectrogram code.

---
+++ Spectrogram/spectrogram_total.py
@@ -0,0 +1,16 @@
+import digital_rf
+import datetime
+import dateutil.parser
+import calendar
+
+import numpy as np
+from matplotlib import pyplot as plt
+import 
+
+# read the data into a vector
+
+# split the data into channels by frequency (PFB)
+
+# make a data set for each channel
+
+# graph
