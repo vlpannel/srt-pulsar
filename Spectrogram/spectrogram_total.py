08af52bd08ea5cdc9f328009fc975d14b22ad465
vlpannel <103673975+vlpannel@users.noreply.github.com>
2022-06-24 17:08:09 -0400

    Played with number of samples to use for spectrogram_total.
    Too big caused lag. Also, why am I seeing dispersion but of lower frequencies arriving first? RFI?

--- Spectrogram/spectrogram_total.py
+++ Spectrogram/spectrogram_total.py
@@ -5,12 +5,19 @@ import calendar
 
 import numpy as np
 from matplotlib import pyplot as plt
+import scipy
+from scipy import signal
+
+##############################################################
+# This code is copied from... 
+##############################################################
 
 # variables
 top_lvl = '/Volumes/NO NAME/pulsar/2022-05-26/rf_data'	# top-level filepath for RF data
 channel = 'misa-l2'	# channel to read from
 pulsar_freq = 29.7	# frequency (Hz) of how often pulsar pulses
-nsamp = 1000000	# number of samples to read
+nsamp = 100000	# number of samples to read
+center_freq = 440000000
 
 # read the data into a vector
 do = digital_rf.DigitalRFReader(top_lvl)
@@ -22,13 +29,16 @@ s, e = do.get_bounds(channel)
 #nsamp = (e - s) / 2		# re-set number of samples based on bounds
 prop = do.get_properties(channel)
 samp_rate = prop['sample_rate_numerator'] / prop['sample_rate_denominator']
-dvec = do.read_vector_c81d(s, nsamp, channel)
+nsamp = samp_rate * 10
+dvec = do.read_vector_c81d(s + (samp_rate * 300), nsamp, channel)
 
 # split the data into channels by frequency (PFB)
 
+
 # make a data set for each channel
 
+
 # graph
-dvec = dvec**2
-plt.specgram(dvec, Fs=samp_rate)
+# This is using pyplot's specgram function to do all the work for me
+plt.specgram(dvec, Fs=samp_rate, Fc=center_freq)
 plt.show()
