a02a78dba8e28ec21597c37c609df9b658416093
vlpannel <103673975+vlpannel@users.noreply.github.com>
2022-06-30 14:11:36 -0400

    Testing with DRF Sink AND Source

--- Testing DRF Scripts/testing_radioconda.py
+++ Testing DRF Scripts/testing_radioconda.py
@@ -5,11 +5,10 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: DRF To File
-# Author: vivelpanel
-# GNU Radio version: 3.9.5.0
+# Title: Testing Radioconda
+# GNU Radio version: 3.10.3.0
 
-from distutils.version import StrictVersion
+from packaging.version import Version as StrictVersion
 
 if __name__ == '__main__':
     import ctypes
@@ -21,29 +20,30 @@ if __name__ == '__main__':
         except:
             print("Warning: failed to XInitThreads()")
 
+from PyQt5 import Qt
+from gnuradio import qtgui
+from gnuradio.filter import firdes
+import sip
+from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import gr
-from gnuradio.filter import firdes
 from gnuradio.fft import window
 import sys
 import signal
-from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-import gr_digital_rf
-import numpy as np; import gr_digital_rf
 
 
 
 from gnuradio import qtgui
 
-class top_block(gr.top_block, Qt.QWidget):
+class testing_radioconda(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "DRF To File", catch_exceptions=True)
+        gr.top_block.__init__(self, "Testing Radioconda", catch_exceptions=True)
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("DRF To File")
+        self.setWindowTitle("Testing Radioconda")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -61,7 +61,7 @@ class top_block(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.settings = Qt.QSettings("GNU Radio", "testing_radioconda")
 
         try:
             if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -79,69 +79,37 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self.gr_digital_rf_digital_rf_source_0 = gr_digital_rf.digital_rf_source(
-            '/Volumes/NO NAME/pulsar/2022-05-26/rf_data',
-            channels=[
-                'misa-l2',
-            ],
-            start=[
-                int(1653595200000000),
-            ],
-            end=[
-                int(1653602399999999),
-            ],
-            repeat=True,
-            throttle=False,
-            gapless=False,
-            min_chunksize=None,
+        self.qtgui_sink_x_0 = qtgui.sink_c(
+            1024, #fftsize
+            window.WIN_BLACKMAN_hARRIS, #wintype
+            0, #fc
+            samp_rate, #bw
+            "", #name
+            True, #plotfreq
+            True, #plotwaterfall
+            True, #plottime
+            True, #plotconst
+            None # parent
         )
-        self.gr_digital_rf_digital_rf_sink_0 = gr_digital_rf.digital_rf_sink(
-            '/Users/vivelpanel/Desktop/SRT UROP 2022/Testing DRF Scripts/rtl-output/2xDrfout',
-            channels=[
-                'ch0',
-            ],
-            dtype=np.int16,
-            subdir_cadence_secs=3600,
-            file_cadence_millisecs=1000,
-            sample_rate_numerator=int(samp_rate),
-            sample_rate_denominator=1,
-            start=0,
-            ignore_tags=False,
-            is_complex=False,
-            num_subchannels=1,
-            uuid_str='adsf',
-            center_frequencies=(
-                None
-            ),
-            metadata={},
-            is_continuous=True,
-            compression_level=0,
-            checksum=False,
-            marching_periods=True,
-            stop_on_skipped=False,
-            stop_on_time_tag=False,
-            debug=False,
-            min_chunksize=None,
-        )
-        self.blocks_vector_to_streams_0 = blocks.vector_to_streams(gr.sizeof_short*1, 2)
-        self.blocks_throttle_0_0 = blocks.throttle(gr.sizeof_short*1, samp_rate,True)
-        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_short*1)
-        self.blocks_file_sink_0_0 = blocks.file_sink(gr.sizeof_short*1, '/Users/vivelpanel/Desktop/SRT UROP 2022/Testing DRF Scripts/rtl-output/source_block_output', False)
-        self.blocks_file_sink_0_0.set_unbuffered(False)
+        self.qtgui_sink_x_0.set_update_time(1.0/10)
+        self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.qwidget(), Qt.QWidget)
+
+        self.qtgui_sink_x_0.enable_rf_freq(False)
+
+        self.top_layout.addWidget(self._qtgui_sink_x_0_win)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 1000, 1, 0, 0)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_throttle_0_0, 0), (self.blocks_file_sink_0_0, 0))
-        self.connect((self.blocks_throttle_0_0, 0), (self.gr_digital_rf_digital_rf_sink_0, 0))
-        self.connect((self.blocks_vector_to_streams_0, 1), (self.blocks_null_sink_0, 0))
-        self.connect((self.blocks_vector_to_streams_0, 0), (self.blocks_throttle_0_0, 0))
-        self.connect((self.gr_digital_rf_digital_rf_source_0, 0), (self.blocks_vector_to_streams_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.qtgui_sink_x_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.settings = Qt.QSettings("GNU Radio", "testing_radioconda")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
@@ -153,12 +121,14 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.blocks_throttle_0_0.set_sample_rate(self.samp_rate)
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.qtgui_sink_x_0.set_frequency_range(0, self.samp_rate)
 
 
 
 
-def main(top_block_cls=top_block, options=None):
+def main(top_block_cls=testing_radioconda, options=None):
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
